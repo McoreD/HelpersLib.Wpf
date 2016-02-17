ed20845b16fcf4c6e7e3b0d0b346e4675a518094
Michael Delpach <mcored@gmail.com>
2016-02-18 05:20:25 +0800

    HelpersLib for WPF Applications

---
+++ HelpersLib/Dialogs/CustomMessageBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HelpersLib
+{
+    public partial class CustomMessageBox : UserControl
+    {
+        public CustomMessageBox(string text, string button1 = "Ok", string button2 = "", string button3 = "")
+        {
+            InitializeComponent();
+            Text.Text = text;
+            Button1.Content = button1;
+
+            if (!string.IsNullOrEmpty(button2))
+            {
+                Button2.Visibility = Visibility.Visible;
+                Button2.Content = button2;
+            }
+
+            Button2.Content = button2;
+            if (!string.IsNullOrEmpty(button3))
+            {
+                Button3.Visibility = Visibility.Visible;
+                Button3.Content = button3;
+            }
+        }
+    }
+}
